0b97ca44e026f683dcc339de1d673dfbccb3d9c4
Nguyen Duy Toan <duytoan.ng1@gmail.com>
2024-05-18 14:47:43 +0700

    - impl bottom navigation bar

--- app/src/main/java/com/shrc/dtoanng/hilt_mvvm_compose_the_movie_app/core/presentation/MainActivity.kt
+++ app/src/main/java/com/shrc/dtoanng/hilt_mvvm_compose_the_movie_app/core/presentation/MainActivity.kt
@@ -6,15 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.fragment.NavHostFragment
-import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.movieList.presentation.MovieListViewModel
+import androidx.navigation.navArgument
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.movieList.util.Screen
 import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.theme.HiltmvvmcomposethemovieappTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,14 +27,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HiltmvvmcomposethemovieappTheme {
-                // A surface container using the 'background' color from the theme
+                SetBarColor(color = MaterialTheme.colorScheme.inverseOnSurface)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.rout
+                    ) {
+                        composable(Screen.Home.rout) {
+                            HomeScreen(navController = navController)
+                        }
 
+                        composable(
+                            route = Screen.Details.rout + "/{movieId}",
+                            arguments = listOf(
+                                navArgument("movieId") {
+                                    type = NavType.IntType
+                                },
+                            ),
+                        ) {
+
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    @Composable
+    fun SetBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        LaunchedEffect(key1 = color) {
+            systemUiController.setSystemBarsColor(color)
         }
     }
 }
