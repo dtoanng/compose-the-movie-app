091c36b921750c213bf42389e00e6af533031326
Nguyen Duy Toan <duytoan.ng@samsung.com>
2024-05-23 07:36:11 +0700

    - modify displaying movie items

--- app/src/main/java/com/shrc/dtoanng/hilt_mvvm_compose_the_movie_app/ui/screens/home/programs/ProgramsScreen.kt
+++ app/src/main/java/com/shrc/dtoanng/hilt_mvvm_compose_the_movie_app/ui/screens/home/programs/ProgramsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -19,13 +18,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.R
 import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.components.MovieItem
-import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.components.MovieItemLandscape
+import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.components.MovieItemPoster
 import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.screens.home.base.BaseMovieListUiEvent
 import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.screens.home.nowplaying.NowPlayingMoviesViewModel
 import com.shrc.dtoanng.hilt_mvvm_compose_the_movie_app.ui.screens.home.popular.PopularMoviesViewModel
@@ -74,12 +75,13 @@ private fun PopularMovieRow(navHostController: NavHostController) {
             color = Color.White,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            text = "Popular movies on Netflix",
+            text = stringResource(R.string.what_popular),
             modifier = Modifier.padding(vertical = 14.dp, horizontal = 8.dp)
         )
         LazyHorizontalGrid(
             rows = GridCells.Fixed(1),
             modifier = Modifier.height(350.dp),
+            contentPadding = PaddingValues(vertical = 1.dp, horizontal = 1.dp)
         ) {
             items(movieListState.movieList.size) { index ->
                 MovieItem(
@@ -112,7 +114,7 @@ private fun NowPlayingMovieRow(navHostController: NavHostController) {
             color = Color.White,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            text = "Now Playing on Netflix",
+            text = stringResource(R.string.now_playing),
             modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
         )
         LazyHorizontalGrid(
@@ -121,7 +123,7 @@ private fun NowPlayingMovieRow(navHostController: NavHostController) {
         ) {
             items(movieListState.movieList.size) { index ->
 
-                MovieItemLandscape(
+                MovieItemPoster(
                     movie = movieListState.movieList[index],
                     navHostController = navHostController
                 )
